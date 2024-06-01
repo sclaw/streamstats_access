dc89e9a2c22c6adf806d2e098570102bfdc79f45
sclaw <klawson1@uvm.edu>
2024-06-01 16:11:14 -0400

    remove unused dependancies

--- source/api_client.py
+++ source/api_client.py
@@ -1,7 +1,4 @@
 import aiohttp
-import asyncio
-import requests
-from .config import config
 
 class APIClient:
     """
@@ -11,17 +8,17 @@ class APIClient:
         session (aiohttp.ClientSession): An instance of aiohttp ClientSession.
     """
 
-    def __init__(self):
+    def __init__(self, server_name='prodweba'):
         """
         Initializes the APIClient with an API key and creates an aiohttp ClientSession.
 
         Args:
             api_key (str): The API key for the USGS API.
         """
-        return None
+        self.server_name = server_name
 
 
-    async def get(self, url, params=None):
+    async def get(self, url, params=None, headers=None):
         """
         Fetches data from the specified API endpoint with given parameters.
 
@@ -36,7 +33,7 @@ class APIClient:
             aiohttp.ClientError: If the request fails.
         """
         async with aiohttp.ClientSession() as c:
-            async with c.get(url, params=params) as response:
+            async with c.get(url, params=params, headers=headers) as response:
                 response.raise_for_status()
                 json = await response.json()
                 return json, response.headers
