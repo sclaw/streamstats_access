5ffbd71479762f6e207b98ef65506084ab5dd92d
sclaw <klawson1@uvm.edu>
2024-06-02 15:56:56 -0400

    update to new file

--- sample/mettawee/run.py
+++ sample/mettawee/run.py
@@ -2,8 +2,8 @@ import streamstats_access as ssa
 import os
 
 
-IN_PATH = os.path.join(os.path.dirname(__file__), 'in_pts.gpkg')
+IN_PATH = os.path.join(os.path.dirname(__file__), 'vt_test.gpkg')
 OUT_PATH = os.path.join(os.path.dirname(__file__), 'out_pts.gpkg')
 
 
-ssa.process_batch(IN_PATH, OUT_PATH, 'VT', 'Name')
+ssa.process_batch(IN_PATH, OUT_PATH, rcode='VT', unique_field='UID')
