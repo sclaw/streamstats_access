ccec258395682a180a63f911edb91145d1117ef0
sclaw <klawson1@uvm.edu>
2024-06-03 15:20:51 -0400

    update docstrings

--- streamstats_access/batch_query.py
+++ streamstats_access/batch_query.py
@@ -2,7 +2,7 @@
 Batch Query Module
 
 This module contains the BatchQueryTool class which provides functionality to process batch 
-queries using the USGS Streamstats API.
+queries using the USGS Streamstats API.  This mimics the functionality of the Batch Processor Tool.
 """
 
 import asyncio
@@ -13,10 +13,13 @@ import logging
 
 async def point_worker(in_q, out_q, server_name, max_retries=3):
     """
-    Processes a single point query by querying all necessary apis and saving the result.
+    Processes a single point by querying all necessary APIs and logging the result.
     
     Args:
-        pt (tuple): A tuple containing the query parameters.
+        in_q (asyncio.Queue): The queue containing points to process.
+        out_q (asyncio.Queue): The queue to put processed points in.
+        server_name (str): The name of the server to query.  Essentially, ID for worker.
+        max_retries (int, optional): The maximum number of times to retry a failed request. Defaults to 3.
     """
     while not in_q.empty():
         pt = in_q.get_nowait()
@@ -102,8 +105,15 @@ async def point_worker(in_q, out_q, server_name, max_retries=3):
                              
 async def _process_batch_async(in_path, out_path, rcode, unique_field, parallel=True):
     """
-    Processes the batch queries by querying the API for each entry in the input file and saving 
+    Processes the batch query by querying the API for each point in the input file and saving 
     the results.
+
+    Args:
+        in_path (str): filepath to load points from
+        out_path (str): filepath to save results to
+        rcode (str): the region code to use
+        unique_field (str): the field in the input geospatial file that contains unique identifiers for each point
+        parallel (bool, optional): whether to asynchronously query prodweba and prodwebb. Defaults to True.
     """
     logging.basicConfig(level=logging.INFO, format="%(asctime)s [%(levelname)s] %(message)s", handlers=[logging.FileHandler(os.path.join(os.path.dirname(in_path), 'ssa.log')), logging.StreamHandler()])
     logging.info('Initiating batch query')
@@ -131,8 +141,14 @@ async def _process_batch_async(in_path, out_path, rcode, unique_field, parallel=
 
 def process_batch(in_path, out_path, rcode, unique_field, parallel=True):
     """
-    Processes the batch queries synchronously by querying the API for each entry in the input file 
-    and saving the results.
+    User entrypoint to the batch processor tool.
+
+    Args:
+        in_path (str): filepath to load points from
+        out_path (str): filepath to save results to
+        rcode (str): the region code to use
+        unique_field (str): the field in the input geospatial file that contains unique identifiers for each point
+        parallel (bool, optional): whether to asynchronously query prodweba and prodwebb. Defaults to True.
     """
     asyncio.run(_process_batch_async(in_path, out_path, rcode, unique_field, parallel))
 
